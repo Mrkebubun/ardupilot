aa5a818cabbe3e38fe04d1ac70e25b6e58e55b8e
dgrat <dgdanielf@gmail.com>
2016-04-16 11:58:29 +0200

    AP_Math: Replaced all wrap_* functions Replaced all wrap_* functions with faster and safer template functions. I want to mention here, that the implementation of wrap_PI is entirely bad, slow and leads to rounding errors. Included is also the 1.5k binary size hack, using template specialization suggested by Lucas.

--- libraries/AP_Math/AP_Math.cpp
+++ libraries/AP_Math/AP_Math.cpp
@@ -49,3 +49,28 @@ float linear_interpolate(float low_output, float high_output,
     return low_output + p * (high_output - low_output);
 }
 
+/*
+ * Wrap an angle in centi-degrees
+ */
+template <class T>
+auto wrap_360_cd(const T &angle) -> decltype(wrap_360(angle, 100.f)) {
+    return wrap_360(angle, 100.f);
+}
+
+template auto  wrap_360_cd<float>   (const float        &angle) -> decltype(wrap_360(angle, 100.f));
+template auto  wrap_360_cd<double>  (const double       &angle) -> decltype(wrap_360(angle, 100.f));
+template auto  wrap_360_cd<int16_t> (const int16_t      &angle) -> decltype(wrap_360(angle, 100.f));
+template auto  wrap_360_cd<int32_t> (const int32_t      &angle) -> decltype(wrap_360(angle, 100.f));
+
+/*
+ * Wrap an angle in centi-degrees
+ */
+template <class T>
+auto wrap_180_cd(const T &angle) -> decltype(wrap_180(angle, 100.f)) {
+    return wrap_180(angle, 100.f);
+} 
+
+template auto  wrap_180_cd<float>   (const float        &angle) -> decltype(wrap_180(angle, 100.f));
+template auto  wrap_180_cd<double>  (const double       &angle) -> decltype(wrap_180(angle, 100.f));
+template auto  wrap_180_cd<int16_t> (const int16_t      &angle) -> decltype(wrap_180(angle, 100.f));
+template auto  wrap_180_cd<int32_t> (const int32_t      &angle) -> decltype(wrap_180(angle, 100.f));
