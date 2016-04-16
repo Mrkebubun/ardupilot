30e64a1b6680b1930c867e6161f122e672f27d28
dgrat <dgdanielf@gmail.com>
2016-04-16 11:59:50 +0200

    AP_Math: Replace linear_interpolate Relaced with template functions and added a check to avoid division by zero.

--- libraries/AP_Math/AP_Math.cpp
+++ libraries/AP_Math/AP_Math.cpp
@@ -1,22 +1,6 @@
 #include "AP_Math.h"
 #include <float.h>
 
-/*
-  linear interpolation based on a variable in a range
- */
-float linear_interpolate(float low_output, float high_output,
-                         float var_value,
-                         float var_low, float var_high)
-{
-    if (var_value <= var_low) {
-        return low_output;
-    }
-    if (var_value >= var_high) {
-        return high_output;
-    }
-    float p = (var_value - var_low) / (var_high - var_low);
-    return low_output + p * (high_output - low_output);
-}
 
 /*
  * Wrap an angle in centi-degrees
@@ -43,3 +27,4 @@ template auto  wrap_180_cd<float>   (const float        &angle) -> decltype(wrap
 template auto  wrap_180_cd<double>  (const double       &angle) -> decltype(wrap_180(angle, 100.f));
 template auto  wrap_180_cd<int16_t> (const int16_t      &angle) -> decltype(wrap_180(angle, 100.f));
 template auto  wrap_180_cd<int32_t> (const int32_t      &angle) -> decltype(wrap_180(angle, 100.f));
+
